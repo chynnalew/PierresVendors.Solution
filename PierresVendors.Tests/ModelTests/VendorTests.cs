c669b18b3583c0798ead5cb49f1eee6cb79b79c5
chynnalew <chynnalew@yahoo.com>
2021-10-01 09:26:58 -0700

    pass test to store strings in vendor constructor

--- PierresVendors.Tests/ModelTests/VendorTests.cs
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -9,8 +9,20 @@ namespace PierresVendors.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("Vendor", "Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void VendorConstructor_ReadsVendorName_String()
+    {
+      Vendor newVendor = new Vendor("Vendor", "Description");
+      Assert.AreEqual("Vendor", newVendor.Name);
+    }
+    [TestMethod]
+    public void VendorConstructor_ReadsVendorDescription_String()
+    {
+      Vendor newVendor = new Vendor("Vendor", "Description");
+      Assert.AreEqual("Description", newVendor.Description);
     }
   }
 }
--- PierresVendors/Models/Vendor.cs
+++ PierresVendors/Models/Vendor.cs
@@ -2,9 +2,13 @@ namespace PierresVendors.Models
 {
   public class Vendor
   {
-    public Vendor ()
+    public string Name {get; set;}
+    public string Description {get; set;}
+
+    public Vendor (string name, string description)
     {
-      
+      Name = name;
+      Description = description;
     }
   }
 }
