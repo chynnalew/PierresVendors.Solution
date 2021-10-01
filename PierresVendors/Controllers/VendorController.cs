e9c3fd260d9bfed720c6cfcd4f029eae1622028c
chynnalew <chynnalew@yahoo.com>
2021-10-01 10:37:30 -0700

    add UI to view vendor list and add vendor

---
+++ PierresVendors/Controllers/VendorController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresVendors.Models;
+
+namespace PierresVendors.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetList();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendor")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor= new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+  }
+}
