469bcf87b7c0f947a2cb8e74b69fcdb8b94655ec
chynnalew <chynnalew@yahoo.com>
2021-10-01 08:17:19 -0700

    create file tree

---
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendors.Models;
+
+namespace PierresVendors.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+    [TestMethod]
+    public void NameOfMethod_DescribeBehavior_ExpectedResult()
+    {
+
+    }
+
+  }
+}
