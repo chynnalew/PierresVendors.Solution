422e961dba2802199b292c9bf61dcf3ddfd08fd1
chynnalew <chynnalew@yahoo.com>
2021-10-01 11:15:56 -0700

    pass order tests to add unique id

--- PierresVendors.Tests/ModelTests/OrderTests.cs
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,13 @@ using System.Collections.Generic;
 namespace PierresVendors.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests :IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
@@ -58,19 +63,63 @@ namespace PierresVendors.Tests
       Order newOrder = new Order(title, description, price, date);
       Assert.AreEqual(date, newOrder.Date);
     }
-    // [TestMethod]
-    // public void GetList_ReadsEmptyList_List()
-    // {
-    //   List<Order> emptyList = new List<Order>{};
-    //   List<Order> testList = Order.GetList();
-    //   CollectionAssert.AreEqual(emptyList,testList);
-    // }
-    // [TestMethod]
-    // public void OrderConstructor_AutoAddsOrderToList_List()
-    // {
-    //   List<Order> emptyList = new List<Order>{};
-    //   List<Order> testList = Order.GetList();
-    //   CollectionAssert.AreEqual(emptyList,testList);
-    // }
+    [TestMethod]
+    public void GetList_ReadsEmptyList_List()
+    {
+      List<Order> emptyList = new List<Order>{};
+      List<Order> testList = Order.GetList();
+      CollectionAssert.AreEqual(emptyList,testList);
+    }
+    [TestMethod]
+    public void OrderConstructor_AutoAddsOrderToList_List()
+    {
+      string title = "title";
+      string description = "description";
+      int price = 10;
+      string date = "date";
+      Order newOrder = new Order(title, description, price, date);
+      List<Order> newList = new List<Order>{};
+      List<Order> testList = Order.GetList();
+      newList.Add(newOrder);
+      CollectionAssert.AreEqual(newList,testList);
+    }
+    [TestMethod]
+    public void ClearAll_ClearsThePrivateList_True()
+    {
+      string title = "title";
+      string description = "description";
+      int price = 10;
+      string date = "date";
+      Order newOrder = new Order(title, description, price, date);
+      Order.ClearAll();
+      List<Order> testList = Order.GetList();
+      List<Order> emptyList = new List<Order> {};
+      CollectionAssert.AreEqual(emptyList, testList);
+    }
+    [TestMethod]
+    public void OrderConstructor_ConstructorAssignsUniqueId_Int()
+    {      
+      string title = "title";
+      string description = "description";
+      int price = 10;
+      string date = "date";
+      Order newOrder = new Order(title, description, price, date);
+      Assert.AreEqual(0, newOrder.Id);
+    }
+    [TestMethod]
+    public void OrderConstructor_ConstructorAssignsUniqueIds_Int()
+    {      
+      string title = "title";
+      string description = "description";
+      int price = 10;
+      string date = "date";
+      Order newOrder = new Order(title, description, price, date);
+      string title2 = "title";
+      string description2 = "description";
+      int price2 = 10;
+      string date2 = "date";
+      Order newOrder2 = new Order(title2, description2, price2, date2);
+      Assert.AreEqual(1, newOrder2.Id);
+    }
   }
 }
